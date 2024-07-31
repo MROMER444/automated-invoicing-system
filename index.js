1dd47efd5722dbe691c4d9cfaca9ccb713ee5b5c
MROMER444 <omerraad444@gmail.com>
2024-07-31 15:52:36 +0300

    complete the rest of categoryController 🚀

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const productController = require("./src/controllers/productController");
+const categoryController = require("./src/controllers/categoryController");
 
 app.use(express.json());
 app.use("/api", productController);
+app.use("/api", categoryController);
 
 const PORT = process.env.PORT || 2000;
 app.listen(PORT, () => console.log('Listening on port', PORT));
--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -4,6 +4,116 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const Joi = require("joi");
 
+router.post('/v1/create-category', async (req, res) => {
+    try {
+        const { error } = categoryValidation(req.body);
+        if (error) {
+            return res.status(400).json({ 'error': error.details[0].message });
+        }
+        let { name, description } = req.body;
+        let existingCategory = await prisma.category.findFirst({
+            where: { name: name }
+        });
+        if (existingCategory) {
+            return res.status(400).json({ message: 'Category already exists' });
+        }
+        const newCategory = await prisma.category.create({
+            data: {
+                name,
+                description,
+            },
+        });
+        if (!newCategory) {
+            return res.status(400).json({ message: 'Category cannot be created' });
+        }
+        return res.status(201).json({ message: 'Category created successfully', category: newCategory });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server error');
+    }
+});
+
+router.put('/v1/update-category/:id' , async(req , res) => {
+    try {
+        const { error } = categoryValidation(req.body);
+        if(error){
+            return res.status(400).json({ 'error': error.details[0].message });
+        }
+        let { id }  = req.params;
+        id = parseInt(id)
+
+        if(isNaN(id)){
+            return res.status(404).json({message : "you have to set an id for the category"})
+        };
+        let { name, description } = req.body;
+        
+    
+        let existingCategory = await prisma.category.findUnique({
+            where : {id : id},
+        });
+        if(!existingCategory){
+            return res.status(404).json({message : `category with this id ${id} not found to update`})
+        };
+        const updatedCategory = await prisma.category.update({
+            where : {id : existingCategory.id},
+            data : {
+                    name: name,
+                    description: description,
+            }
+        });
+        if(updatedCategory){
+            return res.status(200).json({ message: 'Product updated successfully', product: updatedCategory });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server error');
+    }
+
+});
+
+router.get('/v1/get-categories', async (req, res) => {
+    try {
+        let category = await prisma.category.findMany();
+        if (category.length === 0) {
+            return res.status(200).json({ message: 'There are no category' });
+        } else {
+            return res.status(200).json({ categories: category });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server error');
+    }
+});
+
+
+router.delete('/v1/delete-categories/:id' , async (req , res) => {
+    try {
+        const { id } = req.params;
+        const categoryId = parseInt(id);
+        if(isNaN(id)){
+            return res.status(404).json({message : "you have to set an id for the category"})
+        };
+
+        let existingcategory = await prisma.category.findUnique({
+            where : {id : categoryId},
+        });
+        if(!existingcategory){
+            return res.status(404).json({message : `category with this id ${id} not found to delete`})
+        };
+        delete_category = await prisma.category.delete({
+            where:{id : categoryId},
+        });
+        if(!delete_category){
+            return res.status(404).json({message : `category with this id ${id} can't be deleted`})
+        }
+        return res.status(404).json({message : `category deleted`})
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server error');
+    }
+});
 
 
 
@@ -12,18 +122,12 @@ const Joi = require("joi");
 
 
 
-
-
-
-
-
-
-function vategoryValidation(category) {
-    const schema = {
+function categoryValidation(category) {
+    const schema = Joi.object({
         name: Joi.string().min(3).max(30).required(),
         description: Joi.string().min(20).max(250).required(),
-    }
-    return Joi.validate(category, schema);
+    });
+    return schema.validate(category);
 }
 
 module.exports = router;
